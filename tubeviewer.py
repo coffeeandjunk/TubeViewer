aca35ce0c5c5858c0f053579b9368971a9c3af18
Abhishek Chakraborty <abhishek.mech011@gmail.com>
2019-07-09 23:48:16 +0530

    Implemented Scheduler. Implemented an array of video URLs to pick randomly.

--- tubeviewer.py
+++ tubeviewer.py
@@ -1,6 +1,9 @@
+import schedule
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from random import randint, sample
+from videos import videos
 
 chromedriver = './drivers/chromedriver'
 options = webdriver.ChromeOptions()
@@ -9,18 +12,34 @@ options.add_argument('headless')
 driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
 
 # driver = webdriver.Chrome("./drivers/chromedriver")
-driver.implicitly_wait(30)
+# driver.implicitly_wait(10)
 # driver.maximize_window()
 
-try:
-    view = 0
-    print("YT Viewer has started")
-    while( view < 100):
-        driver.get("https://youtu.be/V7sde1A0mzE")
-        time.sleep(474)
-        view = view + 1
-        print(view, " view(s)")
-        driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'r')
+print("TubeViewer has started")
 
-finally:
-    driver.quit()
+def job():
+
+    try:
+        view = 0
+        current_video = sample(videos, 1)[0]
+        print("Viewing ", current_video)
+        driver.get(current_video)
+
+        while(view < randint(1, 5)):
+            time.sleep(5)
+            view = view + 1
+            print(view, " view(s)")
+            driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'r')
+
+    finally:
+        print("Sleeping...")
+        time.sleep(10)
+        print("TubeViewer is awake")
+        # driver.quit()
+
+# schedule.every(randint(7, 17)).minutes.do(job)
+schedule.every(5).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
