86bdf204db161d6cc901d872c8a2cdbf2d19345d
Abhishek Chakraborty <abhishek.mech011@gmail.com>
2019-07-10 00:45:55 +0530

    Implemented realtime duration tracknig. After one video is done playing the system goes to the YouTube homepage and sleeps there. They comes back for a new video.

--- tubeviewer.py
+++ tubeviewer.py
@@ -9,10 +9,10 @@ chromedriver = './drivers/chromedriver'
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
-driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+# driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
 
-# driver = webdriver.Chrome("./drivers/chromedriver")
-# driver.implicitly_wait(10)
+driver = webdriver.Chrome("./drivers/chromedriver")
+driver.implicitly_wait(10)
 # driver.maximize_window()
 
 print("TubeViewer has started")
@@ -22,20 +22,31 @@ def job():
     try:
         view = 0
         current_video = sample(videos, 1)[0]
-        print("Viewing ", current_video)
+        print("Viewing: ", current_video)
         driver.get(current_video)
-        driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'r')
-        duration = driver.find_element_by_class_name('ytp-time-duration').text
-        print(duration)
+        driver.implicitly_wait(10)
+        yt_duration = driver.find_element_by_class_name('ytp-time-duration').text
+        # print(yt_duration)
+        minutes = yt_duration[:yt_duration.find(":")]
+        # print(minutes)
+        seconds = yt_duration[-2:]
+        # print(seconds)
+        duration = (int(minutes) * 60) + int(seconds)
+        print("Duration: ", duration, " secs")
 
-        while(view < randint(1, 5)):
-            time.sleep(5)
+        while(view < randint(1, 3)):
+        # while(view < 5):
+            time.sleep(duration)
+            # time.sleep(5)
             view = view + 1
             print(view, " view(s)")
+            driver.refresh()
 
     finally:
-        print("Sleeping...")
-        time.sleep(10)
+        driver.find_element_by_id('logo-icon-container').click()
+        print("Went to YouTube home screen. Now sleeping...")
+        time.sleep(5)
+        # time.sleep(randint(10, 25))
         print("TubeViewer is awake")
         # driver.quit()
 
