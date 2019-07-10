a77dbed9a88de3269452d0c13f33bbc39193c641
Abhishek Chakraborty <abhishek@Abhisheks-MacBook-Pro.local>
2019-07-10 23:27:43 +0530

    Made the browser quit after every view.

--- tubeviewer.py
+++ tubeviewer.py
@@ -5,25 +5,21 @@ from selenium.webdriver.common.keys import Keys
 from random import randint, sample
 from videos import videos
 
-chromedriver = './drivers/chromedriver'
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-
-driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
-
-# driver = webdriver.Chrome("./drivers/chromedriver")
-driver.implicitly_wait(10)
-# driver.maximize_window()
-
-print("TubeViewer has started")
-
 def job():
 
     try:
-        view = 0
+        chromedriver = './drivers/chromedriver'
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+
+        driver = webdriver.Chrome(executable_path=chromedriver, options=options)
+        driver.implicitly_wait(10)
+        print("TubeViewer has started")
+
         current_video = sample(videos, 1)[0]
-        print("Viewing: ", current_video)
         driver.get(current_video)
+        print("Viewing: ", current_video)
+
         driver.implicitly_wait(10)
         yt_duration = driver.find_element_by_class_name('ytp-time-duration').text
         # print(yt_duration)
@@ -34,24 +30,20 @@ def job():
         duration = (int(minutes) * 60) + int(seconds)
         print("Duration: ", duration, " secs")
 
-        while(view < randint(1, 3)):
-        # while(view < 5):
-            time.sleep(duration)
-            # time.sleep(5)
-            view = view + 1
-            print(view, " view(s)")
-            driver.refresh()
+        time.sleep(duration)
+        # time.sleep(10)
+
+        driver.find_element_by_id('logo-icon-container').click()
+        print("Went to YouTube home screen.")
+        time.sleep(randint(120, 240))
+        # time.sleep(10)
 
     finally:
-        driver.find_element_by_id('logo-icon-container').click()
-        print("Went to YouTube home screen. Now sleeping...")
-        # time.sleep(5)
-        time.sleep(randint(120, 240))
-        print("TubeViewer is awake")
-        # driver.quit()
+        print("TubeViewer has quit.")
+        driver.quit()
 
-schedule.every(randint(5, 10)).minutes.do(job)
-# schedule.every(5).seconds.do(job)
+schedule.every(randint(15, 30)).minutes.do(job)
+# schedule.every(10).seconds.do(job)
 
 while True:
     schedule.run_pending()
